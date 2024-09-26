44400d6cad9a90488dd94f1b679973e30c9ed681
Chad <chad.golden87@gmail.com>
2024-09-26 13:26:31 -0400

    added Books and Breakfast Louisville

---
+++ The Big Capstone/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Books and Breakfast Louisville</h1>
+  </header>
+
+  
+</template>
+
+<style scoped>
+
+</style>
